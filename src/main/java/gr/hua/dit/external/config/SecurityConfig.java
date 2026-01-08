5dd1830d19f535f51fd9dc27d57328bbc92393ec
Stavroula Parsali <it2022088@hua.gr>
2026-01-08 18:22:45 +0200

    Secure calls

--- src/main/java/gr/hua/dit/external/config/SecurityConfig.java
+++ src/main/java/gr/hua/dit/external/config/SecurityConfig.java
@@ -1,20 +1,62 @@
 package gr.hua.dit.external.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value("{$app.api-key}")
+    private String validApiKey;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new ApiKeyAuthFilter(validApiKey), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 );
         return http.build();
+    }
+
+    public static class ApiKeyAuthFilter extends OncePerRequestFilter {
+        private final String validApiKey;
+
+        public ApiKeyAuthFilter(String validApiKey) {
+            this.validApiKey = validApiKey;
+        }
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+
+            String requestKey = request.getHeader("SFGO-API-KEY");
+
+            if (validApiKey.equals(requestKey)) {
+                var auth = new UsernamePasswordAuthenticationToken("api-service", null, Collections.emptyList());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                filterChain.doFilter(request, response);
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); //401
+                response.getWriter().write("Unauthorized: Invalid or missing API Key");
+            }
+        }
     }
 
 }
